822c5a1a30a5ce97d8047ef1d899e8bb06f0f1a7
Stuti Sharma <stuti105sharma@gmail.com>
2024-01-14 17:30:07 +0530

    completed 01-js/hard

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,51 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(num) {
+    this.result += num;
+  }
+
+  subtract(num) {
+    this.result -= num;
+  }
+
+  multiply(num) {
+    this.result *= num;
+  }
+
+  divide(num) {
+    if (num === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result /= num;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    if (!/^[0-9+\-*/().]+$/.test(expression.replace(/\s/g, ''))) {
+      throw new Error("Invalid characters in the expression");
+    }
+    try {
+      this.result = eval(expression);
+      if (!isFinite(this.result)) {
+        throw new Error("Invalid result");
+      }
+    } catch (error) {
+      throw new Error("Invalid expression");
+    }
+  }
+}
 
 module.exports = Calculator;
